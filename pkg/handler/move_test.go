e4792ad927a490daf3f6a30c49050b5c3be1aa8f
monirz <monir.smith@gmail.com>
2019-08-19 14:01:29 +0600

    test case added for move with date2str function

--- pkg/handler/move_test.go
+++ pkg/handler/move_test.go
@@ -15,4 +15,16 @@ func TestMove(t *testing.T) {
 	if d.Vars["VAR1"] != "TEST DATA" {
 		t.Errorf("Expected %v got %v", "TEST DATA", d.Vars["VAR1"])
 	}
+
+	//test move with date2str function
+
+	dateStr := "MOVE DATE2STR(\"08/07/2019\", \"yy-mm-dd\") TO VAR1"
+
+	d.Move(dateStr)
+
+	expected := "2019-Aug-07"
+	if d.Vars["VAR1"] != expected {
+		t.Errorf("Expected %v, got %v", expected, d.Vars["VAR1"])
+	}
+
 }
