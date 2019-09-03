d7302dea18f9ee6f94a608618c0b5bca9ef1f209
monirz <monir.smith@gmail.com>
2019-09-03 18:25:58 +0600

    CSV to JSON write with PERFORM loop added

--- pkg/handler/perform_read_csv.go
+++ pkg/handler/perform_read_csv.go
@@ -85,31 +85,49 @@ func (d *Data) performReadCSV(tokens []string, fileReference, recordName string)
 					fileName = fileName[1 : len(fileName)-1]
 				}
 
-				if writeCount < 1 {
+				if strings.ToLower(tokens[i+6]) == "csv" {
 
-					outFile, err = os.OpenFile(fileName, os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0644)
+					if writeCount < 1 {
+
+						outFile, err = os.OpenFile(fileName, os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0644)
+
+						if err != nil {
+							fmt.Println(err)
+							os.Exit(1)
+						}
+						csvWriter = csv.NewWriter(outFile)
+
+					}
+
+					info, _ := outFile.Stat()
+					fileSize := info.Size()
+
+					if fileSize < 1 {
+
+						d.csvWrite(writeTokens, csvWriter, 1)
+					} else {
+
+						d.csvWrite(writeTokens, csvWriter, 2)
+					}
+					csvWriter.Flush()
+
+					//handle and write to json file
+				} else if strings.ToLower(tokens[i+5]) == "json" {
+
+					outFile, err = os.OpenFile("output.json", os.O_RDWR|os.O_CREATE, 0644)
 
 					if err != nil {
 						fmt.Println(err)
 						os.Exit(1)
 					}
-					csvWriter = csv.NewWriter(outFile)
+
+					d.csvToJasonWrite(writeTokens, writeCount, outFile)
+
+					outFile.Close()
 
 				}
-
-				info, _ := outFile.Stat()
-				fileSize := info.Size()
 
 				writeCount++
-
-				if fileSize < 1 {
-
-					d.Write(writeTokens, csvWriter, 1)
-				} else {
-
-					d.Write(writeTokens, csvWriter, 2)
-				}
-				csvWriter.Flush()
 
 			}
 		}
--- pkg/handler/write.go
+++ pkg/handler/write.go
@@ -2,11 +2,13 @@ package handler
 
 import (
 	"encoding/csv"
+	"encoding/json"
+	"fmt"
 	"log"
 	"os"
 )
 
-func (d *Data) Write(tokens []string, csvWriter *csv.Writer, writeCount int) {
+func (d *Data) csvWrite(tokens []string, csvWriter *csv.Writer, writeCount int) {
 
 	writer := csvWriter
 
@@ -43,4 +45,58 @@ func (d *Data) Write(tokens []string, csvWriter *csv.Writer, writeCount int) {
 		log.Println(err)
 		os.Exit(1)
 	}
+}
+
+func (d *Data) csvToJasonWrite(tokens []string, writeCount int, file *os.File) {
+
+	//find the record
+	recordData := d.Record[tokens[1]]
+
+	var jsonRecords = make([]map[string]string, 0)
+
+	for {
+		decoder := json.NewDecoder(file)
+
+		err := decoder.Decode(&jsonRecords)
+
+		if err != nil {
+			if err.Error() == "EOF" {
+
+				break
+			}
+
+			fmt.Println("Error decoding JSON file", err)
+			os.Exit(1)
+		}
+
+	}
+
+	jsonRecords = append(jsonRecords, recordData)
+
+	// create new indented JSON with the added user
+	newBytes, err := json.MarshalIndent(jsonRecords, "", "    ")
+	if err != nil {
+		panic(err)
+	}
+
+	// write the new indented json over original file
+	_, err = file.WriteAt(newBytes, 0)
+	if err != nil {
+		panic(err)
+	}
+
+	// content, err := ioutil.ReadFile("output.json")
+
+	// m := make([]map[string]string, 0)
+
+	// if err = json.Unmarshal(content, &m); err != nil {
+
+	// 	log.Fatal(err)
+
+	// }
+
+}
+
+func (d *Data) csvToFixedWrite(tokens []string, writeCount int) {
+
 }
